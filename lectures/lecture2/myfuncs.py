3af5f5c6a9289532f0986e0e96d50bf7421d5ebf
Guest 01 <anchambers314@gmail.com>
2024-08-29 17:45:13 -0700

    Created logBase2Approx function, which is used to create a more accurate guess in ln function, thus being accurate for extremely large numbers

--- lectures/lecture2/myfuncs.py
+++ lectures/lecture2/myfuncs.py
@@ -1,6 +1,7 @@
 #
 # Module for basic mathematic functions
 #
+import numpy as np
 E = 2.7182818284590451
 MAX_STEPS = 100
 
@@ -28,11 +29,24 @@ def exp(x, kmax = MAX_STEPS):
         s += (z**k)/factorial(k)
     return exp0*s
 
+def logBase2Approx(x):
+    s = int(x + .5)
+    count = 0
+    while(s > 0):
+        s >>= 1
+        count += 1
+    
+    return count
+
+#The approximation for s is based off of the idea presented by in the following link
+#https://www.researchgate.net/post/Is_there_an_approximation_of_lnx_for_a_large_value_of_x#:~:text=ln(x)%20is%20an%20analytic,any%20point%20of%20its%20domain.&text=ln%20x%20%3D%20ln%20a%20%2B%20n,quite%20good%20as%20an%20approximation.
+#The only thing required is a pre-calculation of ln(2) = 0.69314718056.
+#This function should work for all x's
 def ln(x, kmax = MAX_STEPS):
     if(x <= 0):
         print("Invalid input for ln function")
         return 0
-    s = 15
+    s = 0.69314718056*logBase2Approx(x)
     for i in range(kmax):
         s += x * exp(-s) - 1
     return s
