0d6276295d0a0017196bfa565e1e3c42930886d4
Guest 01 <anchambers314@gmail.com>
2024-09-03 10:04:10 -0700

    Added lecture 3 and demo_pythonlists.py file

---
+++ lectures/lecture3/demo_pythonlists.py
@@ -0,0 +1,67 @@
+list = [1, 3, 2, 1]
+print('Practice append')
+print(list)
+list.append(4)
+print(list)
+
+list = [1, 3, 2, 1]
+print('Practice clear')
+print(list)
+list.clear()
+print(list)
+
+list = [1, 3, 2, 1]
+print('Practice copy')
+print(list)
+list2 = list.copy()
+print(list2)
+
+
+list = [1, 3, 2, 1]
+print('Practice count')
+print(list)
+print(list.count(1))
+
+
+list = [1, 3, 2, 1]
+print('Practice extend')
+list2 = [3,4,5]
+print(list)
+print(list2)
+list.extend(list2)
+print(list)
+
+list = [1, 3, 2, 1]
+print('Practice index')
+print(list)
+print(list.index(2))
+
+list = [1, 3, 2, 1]
+print('Practice insert')
+print(list)
+list.insert(1, 'inserted item')
+print(list)
+
+list = [1, 3, 2, 1]
+print('Practice pop')
+print(list)
+print(list.pop())
+print(list)
+
+list = [1, 3, 2, 1]
+print('Practice remove')
+print(list)
+list.remove(2)
+print(list)
+
+list = [1, 3, 2, 1]
+print('Practice reverse')
+print(list)
+list.reverse()
+print(list)
+
+list = [1, 3, 2, 1]
+print('Practice sort')
+print(list)
+list.sort()
+print(list)
