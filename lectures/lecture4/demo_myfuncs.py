9700ce8e4f6889c69ab68b303ee34adace89ab18
Guest 01 <anchambers314@gmail.com>
2024-09-05 20:10:33 -0700

    Updated test

--- lectures/lecture4/demo_myfuncs.py
+++ lectures/lecture4/demo_myfuncs.py
@@ -1,23 +1,24 @@
 import numpy as np
 import myfuncs as fn
 
+
 def testsqrt(test_value):
     actual = np.sqrt(test_value)
-    estimated = fn.sqrt(test_value)
+    estimated = fn.sqrt(test_value, None, 25, 1e-10)
 
     abs_error = actual - estimated
     print("For x =", test_value,"the absolute error between numpy and my created func for sqrt is", abs_error)
 
 def testexp(test_value):
     actual = np.exp(test_value)
-    estimated = fn.exp(test_value)
+    estimated = fn.exp(test_value, 25, 1e-12)
 
     abs_error = actual - estimated
     print("For x =", test_value,"the absolute error between numpy and my created func for exp is", abs_error)
 
 def testln(test_value):
     actual = np.log(test_value)
-    estimated = fn.ln(test_value)
+    estimated = fn.ln(test_value, None, 25)
 
     abs_error = actual - estimated
     print("For x =", test_value,"the absolute error between numpy and my created func for ln is", abs_error)
